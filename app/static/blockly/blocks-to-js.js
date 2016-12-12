2a7c8dc7173728ffd7acc5c81a61579735f1643c
a-haas <antoine.haas@etu.unistra.fr>
2016-12-13 00:20:25 +0100

    blocks : quite good actually

--- app/static/blockly/blocks-to-js.js
+++ app/static/blockly/blocks-to-js.js
@@ -1,5 +1,5 @@
 /* basic step when going forward or backward */
-basicstep = 10;
+basicstep = 25;
 
 Blockly.JavaScript['start'] = function(block) {
   var value_start = Blockly.JavaScript.valueToCode(block, 'start', Blockly.JavaScript.ORDER_ATOMIC);
@@ -12,33 +12,42 @@ Blockly.JavaScript['start'] = function(block) {
 
 Blockly.JavaScript['forward'] = function(block) {
   var number_steps = block.getFieldValue('steps');
-  var value_follow = Blockly.JavaScript.valueToCode(block, 'follow', Blockly.JavaScript.ORDER_ATOMIC);
+  var value_follow = Blockly.JavaScript.valueToCode(block, 'follow', Blockly.JavaScript.ORDER_NONE);
   /* get the current position of the character + the additionnal movement required */
-  var newpos = $("#character").position().left - $("#display").position().left;
+  var newpos = basicstep*number_steps;
   // TODO: Assemble JavaScript into code variable.
   var code = 
   '$( "#character" ).animate({' +
-    'left: ' + newpos +
+    'left: "+=' + newpos + '"' +
   '}, {' +
     'duration: 500,' +
-  '});';
+  '});' + value_follow;
   // TODO: Change ORDER_NONE to the correct strength.
   return [code, Blockly.JavaScript.ORDER_NONE];
 };
 
 Blockly.JavaScript['backward'] = function(block) {
   var number_steps = block.getFieldValue('steps');
-  var value_steps = Blockly.JavaScript.valueToCode(block, 'steps', Blockly.JavaScript.ORDER_ATOMIC);
+  var value_steps = Blockly.JavaScript.valueToCode(block, 'steps', Blockly.JavaScript.ORDER_NONE);
   // TODO: Assemble JavaScript into code variable.
-  var code = '...';
+  var newpos = basicstep*number_steps;
+  // TODO: Assemble JavaScript into code variable.
+  var code = 
+  '$( "#character" ).animate({' +
+    'left: "-=' + newpos + '"' +
+  '}, {' +
+    'duration: 500,' +
+  '});' + value_steps;
   // TODO: Change ORDER_NONE to the correct strength.
   return [code, Blockly.JavaScript.ORDER_NONE];
 };
 
 Blockly.JavaScript['speak'] = function(block) {
   var text_speech = block.getFieldValue('speech');
-  var value_speech = Blockly.JavaScript.valueToCode(block, 'speech', Blockly.JavaScript.ORDER_ATOMIC);
-  var code = 'alert("' + text_speech + '");';
+  var value_speech = Blockly.JavaScript.valueToCode(block, 'speech', Blockly.JavaScript.ORDER_NONE);
+  var code = 
+  '$("#speech").text("'+ text_speech +'");' +
+  '$( "#speech" ).show("slow");' + value_speech;
   // TODO: Change ORDER_NONE to the correct strength.
   return [code, Blockly.JavaScript.ORDER_NONE];
 };
